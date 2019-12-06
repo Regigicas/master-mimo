d1894f0d2b43e5e396afd24aa938d738bea26c7d
Regigicas <regigicas@hotmail.es>
2019-12-06 20:34:03 +0100

    Update app de ionic

--- trabajo_ionic/src/app/app-routing.module.ts
+++ trabajo_ionic/src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { EstaLogueadoService } from './servicios/esta-logueado.service';
+import { NoEstaLogueadoService } from './servicios/no-esta-logueado.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./componentes/home/home.module').then( m => m.HomePageModule), canActivate: [EstaLogueadoService]},
-  { path: 'login', loadChildren: () => import('./componentes/login/login.module').then( m => m.LoginPageModule) },
-  { path: 'register', loadChildren: () => import('./componentes/register/register.module').then(m => m.RegisterPageModule) },
+  { path: 'login', loadChildren: () => import('./componentes/login/login.module').then( m => m.LoginPageModule), canActivate: [NoEstaLogueadoService] },
+  { path: 'register', loadChildren: () => import('./componentes/register/register.module').then(m => m.RegisterPageModule), canActivate: [NoEstaLogueadoService] },
   { path: 'plataformas', loadChildren: () => import('./componentes/plataformas/plataformas.module').then(m => m.PlataformasPageModule), canActivate: [EstaLogueadoService] },
   { path: 'plataformas/:id', loadChildren: () => import('./componentes/plataforma/plataforma.module').then(m => m.PlataformaPageModule), canActivate: [EstaLogueadoService] },
   { path: 'usuario', loadChildren: () => import('./componentes/usuario/usuario.module').then(m => m.UsuarioPageModule), canActivate: [EstaLogueadoService] },
   { path: 'juego/:id', loadChildren: () => import('./componentes/juego-info/juego-info.module').then(m => m.JuegoInfoPageModule), canActivate: [EstaLogueadoService] },
   { path: 'favoritos', loadChildren: () => import('./componentes/favoritos/favoritos.module').then(m => m.FavoritosPageModule), canActivate: [EstaLogueadoService] },
   { path: 'juego/plataforma/:id', loadChildren: () => import('./componentes/juegos-plataforma/juegos-plataforma.module').then(m => m.JuegosPlataformaPageModule), canActivate: [EstaLogueadoService] },
-  { path: 'usuario/password', loadChildren: () => import('./componentes/usuario-password/usuario-password.module').then(m => m.UsuarioPasswordPageModule) },
-  { path: 'usuario/datos', loadChildren: () => import('./componentes/usuario-datos/usuario-datos.module').then(m => m.UsuarioDatosPageModule) },
-  { path: 'buscar', loadChildren: () => import('./componentes/buscar/buscar.module').then(m => m.BuscarPageModule) },
-  { path: 'escanear-qr', loadChildren: () => import('./componentes/escanear-qr/escanear-qr.module').then(m => m.EscanearQrPageModule) },
+  { path: 'usuario/password', loadChildren: () => import('./componentes/usuario-password/usuario-password.module').then(m => m.UsuarioPasswordPageModule), canActivate: [EstaLogueadoService] },
+  { path: 'usuario/datos', loadChildren: () => import('./componentes/usuario-datos/usuario-datos.module').then(m => m.UsuarioDatosPageModule), canActivate: [EstaLogueadoService] },
+  { path: 'buscar', loadChildren: () => import('./componentes/buscar/buscar.module').then(m => m.BuscarPageModule), canActivate: [EstaLogueadoService] },
+  { path: 'escanear-qr', loadChildren: () => import('./componentes/escanear-qr/escanear-qr.module').then(m => m.EscanearQrPageModule), canActivate: [EstaLogueadoService] },
   { path: '**', redirectTo: 'home' }
 ];
 
--- trabajo_ionic/src/app/componentes/usuario/usuario.page.ts
+++ trabajo_ionic/src/app/componentes/usuario/usuario.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/modelos/Usuario';
 import { UsuariosService } from 'src/app/servicios/usuarios.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-usuario',
@@ -10,12 +12,39 @@ import { UsuariosService } from 'src/app/servicios/usuarios.service';
 export class UsuarioPage implements OnInit
 {
     usuario: Usuario = null;
-    constructor(private usuarioService: UsuariosService) { }
+    constructor(private usuarioService: UsuariosService, private router: Router,
+        private alertController: AlertController) { }
     
     ngOnInit() {}
 
     ionViewWillEnter()
     {
         this.usuario = this.usuarioService.getUsuarioActivo();
+    }
+
+    async doLogout()
+    {
+        const alert = await this.alertController.create(
+        {
+            message: '¿Estás seguro de que deseas cerrar la sesión?',
+            buttons:
+            [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                },
+                {
+                    text: 'Sí',
+                    handler: () =>
+                    {
+                        this.usuarioService.setUsuarioActivo(null);
+                        this.router.navigate(['/login'], { replaceUrl: true });
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
     }
 }
